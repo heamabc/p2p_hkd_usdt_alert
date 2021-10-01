aea9474232dd61f9fc1ad43f6ee83d1f334e33cd
Ubuntu <ubuntu@ip-172-31-3-1.ap-southeast-1.compute.internal>
2021-10-01 11:39:56 +0800

    update

--- main.py
+++ main.py
@@ -13,9 +13,6 @@ logging.basicConfig(filename=logname,
                             datefmt='%H:%M:%S',
                             level=logging.INFO)
 
-
-
-
 def binance_p2p_scan(telegram_url, group_chat_id):
     payload = {
         "page": 1,
@@ -29,7 +26,13 @@ def binance_p2p_scan(telegram_url, group_chat_id):
         "publisherType": None,
         "merchantCheck": False
     }
-    res = requests.post(r'https://c2c.binance.com/bapi/c2c/v2/friendly/c2c/adv/search/', json=payload)
+    while True:
+        try:
+            res = requests.post(r'https://c2c.binance.com/bapi/c2c/v2/friendly/c2c/adv/search/', json=payload)
+            break
+        except:
+            print('Binance Requests Error')
+            time.sleep(5)
     
     if res.json()['success'] != True:
         logging.info('Binance Error')
@@ -44,7 +47,7 @@ def binance_p2p_scan(telegram_url, group_chat_id):
     
     print_df = []
     for i in range(len(price)):
-        if price[i] <= 7.83 and min_amount[i] <= 2500:
+        if price[i] <= 7.8 and min_amount[i] <= 2500:
             print_df.append([df[0][i], df[1][i], df[2][i]])
 
     number_of_transactions = len(print_df)
@@ -52,14 +55,21 @@ def binance_p2p_scan(telegram_url, group_chat_id):
         min_price = min([ele[0] for ele in print_df])
         
         time_message = f'時間：{datetime.now(pytz.timezone("Asia/Hong_Kong")).strftime("%Y-%m-%d %H:%M:%S")} 最低價錢有 ${min_price}'
-        message = f'Binance 有 {number_of_transactions} 個 quote 既 rate <= 7.83 同埋 minimum <= 2500'
+        message = f'Binance 有 {number_of_transactions} 個 quote 既 rate <= 7.8` 同埋 minimum <= 2500'
         
         headers = ['Rate', 'Min $', 'Max $']
         requests.get(telegram_url + "sendMessage?text={}&chat_id={}&parse_mode=Markdown".format(
             time_message + '\n' + message + '``` \n' + tabulate(print_df, headers= headers,tablefmt='fancy_grid', showindex=False) +'```', group_chat_id))
 
 def aax_p2p_scan(telegram_url, group_chat_id):
-    res = requests.get(r'https://api.aax.com/otc/v2/order/all?coin=USDT&orderType=sell&orderBy=ASC&unit=HKD&paymentMethods=4&amount=-1&page=0&limit=10&region=*&amountType=Ordinary')
+    while True:
+        try:
+            res = requests.get(r'https://api.aax.com/otc/v2/order/all?coin=USDT&orderType=sell&orderBy=ASC&unit=HKD&paymentMethods=4&amount=-1&page=0&limit=10&region=*&amountType=Ordinary')
+            break
+        except:
+            print('AAX Requests Error')
+            time.sleep(5)
+
     res = res.json()
 
     if res['success'] != True:
@@ -72,7 +82,7 @@ def aax_p2p_scan(telegram_url, group_chat_id):
 
     print_df = []
     for i in range(len(price)):
-        if price[i] <= 7.83 and price[i] != 0 and min_amount[i] <= 2500:
+        if price[i] <= 7.8 and price[i] != 0 and min_amount[i] <= 2500:
             print_df.append([price[i], min_amount[i], max_amount[i]])
 
     number_of_transactions = len(print_df)
@@ -80,7 +90,7 @@ def aax_p2p_scan(telegram_url, group_chat_id):
         min_price = min([ele[0] for ele in print_df])
 
         time_message = f'時間：{datetime.now(pytz.timezone("Asia/Hong_Kong")).strftime("%Y-%m-%d %H:%M:%S")} 最低價錢有 {min_price}'
-        message = f'AAX 有 {number_of_transactions} 個 quote 既 rate <= 7.83 同埋 minimum <= 2500 '
+        message = f'AAX 有 {number_of_transactions} 個 quote 既 rate <= 7.8 同埋 minimum <= 2500 '
         
         headers = ['Rate', 'Min $', 'Max $']
         requests.get(telegram_url + "sendMessage?text={}&chat_id={}&parse_mode=Markdown".format(
